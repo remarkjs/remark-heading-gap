1035152cd095f64dbc07cae5dbe6c5e7dd95e22a
Titus Wormer <tituswormer@gmail.com>
2021-08-05 01:09:52 +0200

    Update `xo`

--- test/index.js
+++ test/index.js
@@ -2,18 +2,23 @@
  * @typedef {import('../index.js').Options} Options
  */
 
-import fs from 'fs'
-import path from 'path'
+import fs from 'node:fs'
+import path from 'node:path'
 import test from 'tape'
 import {remark} from 'remark'
 import remarkHeadingGap from '../index.js'
+
+const own = {}.hasOwnProperty
 
 const base = path.join('test', 'fixtures')
 
 /** @type {Record<string, Record<string, string>>} */
 const specs = {}
+const files = fs.readdirSync(base)
+let index = -1
 
-fs.readdirSync(base).forEach((contents) => {
+while (++index < files.length) {
+  const contents = files[index]
   const parts = contents.split('.')
 
   if (!specs[parts[0]]) {
@@ -24,27 +29,32 @@ fs.readdirSync(base).forEach((contents) => {
     path.join(base, contents),
     'utf-8'
   )
-})
+}
 
 test('remark-heading-gap', (t) => {
-  Object.keys(specs).forEach((name) => {
-    const spec = specs[name]
-    /** @type {Options|undefined} */
-    let options
+  /** @type {string} */
+  let name
 
-    if (name === 'three-zero') {
-      options = {3: {before: 0, after: 0}}
+  for (name in specs) {
+    if (own.call(specs, name)) {
+      const spec = specs[name]
+      /** @type {Options|undefined} */
+      let options
+
+      if (name === 'three-zero') {
+        options = {3: {before: 0, after: 0}}
+      }
+
+      t.deepEqual(
+        remark()
+          .use(remarkHeadingGap, options)
+          .processSync(spec.fixture)
+          .toString(),
+        spec.expected,
+        name
+      )
     }
-
-    t.deepEqual(
-      remark()
-        .use(remarkHeadingGap, options)
-        .processSync(spec.fixture)
-        .toString(),
-      spec.expected,
-      name
-    )
-  })
+  }
 
   t.end()
 })
