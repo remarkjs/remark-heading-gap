2181f67397009a9ffd12b386a6f7b082ff5ddc4a
Titus Wormer <tituswormer@gmail.com>
2023-09-23 16:30:03 +0200

    Change to use `exports`

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 /**
- * @typedef {import('../index.js').Options} Options
+ * @typedef {import('remark-heading-gap').Options} Options
  */
 
 import assert from 'node:assert/strict'
@@ -7,11 +7,11 @@ import fs from 'node:fs/promises'
 import process from 'node:process'
 import test from 'node:test'
 import {remark} from 'remark'
-import remarkHeadingGap from '../index.js'
+import remarkHeadingGap from 'remark-heading-gap'
 
 test('remarkHeadingGap', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('../index.js')).sort(), [
+    assert.deepEqual(Object.keys(await import('remark-heading-gap')).sort(), [
       'default'
     ])
   })
